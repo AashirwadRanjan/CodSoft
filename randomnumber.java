9175762718a16e78053a8e106b334d8a26150faf
AashirwadRanjan <85273995+AashirwadRanjan@users.noreply.github.com>
2023-09-03 12:57:41 +0530

    Update randomnumber.java

--- randomnumber.java
+++ randomnumber.java
@@ -1,34 +1,24 @@
 
 import java.util.Scanner;
 
-public class GFG {
+public class randnuguesser {
 
 
-	public static void
-	guessingNumberGame()
+	public static void guessingNumberGame()
 	{
 
 		Scanner sc = new Scanner(System.in);
 
 		
 		int number = 1 + (int)(100* Math.random());
-
-
 		int K = 5;
-
 		int i, guess;
 
-		System.out.println(
-			"A number is chosen"
-			+ " between 1 to 100."
-			+ "Guess the number"
-			+ " within 5 trials.");
-
+		System.out.println("A number is chosen"+" between 1 to 100."+"Guess the number"+" within 5 trials.");
 	
 		for (i = 0; i < K; i++) {
 
-			System.out.println(
-				"Guess the number:");
+			System.out.println("Guess the number:");
 
 		
 			guess = sc.nextInt();
@@ -64,11 +54,8 @@ public class GFG {
 	}
 
 
-	public static void
-	main(String arg[])
+	public static void main(String arg[])
 	{
-
-
 		guessingNumberGame();
 	}
 }
